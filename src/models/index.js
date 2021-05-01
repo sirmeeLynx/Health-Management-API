b2577d79ebb850ee66b9e913b3d997d2adc8809b
johnnair <tobiz2002@gmail.com>
2021-05-01 11:53:32 +0100

    service

--- src/models/index.js
+++ src/models/index.js
@@ -1,2 +1,2 @@
-module.exports.Token = require('./token.model');
-module.exports.User = require('./user.model');
+module.exports.Department = require('./department.model');
+module.exports.ContactPerson = require('./contactperson.model');
